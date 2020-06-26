ae8040d39da1c2e3d0bb4fb6f2ca4c409d8b854b
poonynh_ <poon.roon3@mail.kmutt.ac.th>
2020-06-26 20:01:25 +0700

    [add] container on eventpage

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,6 @@
+var container = 
+'<div class="bg" style="background-image=url(WAIT.png), background-repeat:no-repeat,height: 100%,width: 100% "," >เสี่ยวๆๆๆๆ</span>';
+
 chrome.runtime.onMessage.addListener(function(request ,sender, sendRespond){
     if(request.todo == "showPageAction"){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
@@ -5,7 +8,7 @@ chrome.runtime.onMessage.addListener(function(request ,sender, sendRespond){
             var div = document.createElement('div');
             document.body.appendChild(div);
             div.className = 'kkth';
-            div.innerHTML = '<span class="kkkk">เสี่ยวๆๆๆๆ</span>';
+            div.innerHTML = container;
         })
         alert("Fuck you Sean");
        
