6165f3b9f66628973d526e1c4bf18b12bfb55338
poonynh_ <poon.roon3@mail.kmutt.ac.th>
2020-06-27 00:19:39 +0700

    [update] <div> layer

--- content.js
+++ content.js
@@ -3,4 +3,4 @@
         var div = document.createElement('div');
         document.body.appendChild(div);
         // div.className = 'kkth';
-        div.innerHTML = '<div class="outer"><div class="inner">เสี่ยวๆๆๆๆ</div></div>';
+        div.innerHTML = '<div id="idle"><div class="bg"><div class="outertext"><div class="innertext">เสี่ยวๆๆๆๆ</div></div></div></div>';
