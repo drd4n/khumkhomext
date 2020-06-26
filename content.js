097bf851dccbb3eb87f4479400740f6145fdb13c
Dan Kent <patorn.5@mail.kmutt.ac.th>
2020-06-26 23:59:25 +0700

    [update]change div name

--- content.js
+++ content.js
@@ -3,4 +3,4 @@
             var div = document.createElement('div');
             document.body.appendChild(div);
             // div.className = 'kkth';
-            div.innerHTML = '<div class="kkth"><div class="kkkk">เสี่ยวๆๆๆๆ</div></div>';
+            div.innerHTML = '<div class="outer"><div class="inner">เสี่ยวๆๆๆๆ</div></div>';
