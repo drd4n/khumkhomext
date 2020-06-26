76f3f96ffdd939968a5e4724d3a21c31d5ce2c94
Dan Kent <patorn.5@mail.kmutt.ac.th>
2020-06-27 00:05:28 +0700

    [update]clean code

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // chrome.runtime.sendMessage({todo: "showPageAction"});
 
-            var div = document.createElement('div');
-            document.body.appendChild(div);
-            // div.className = 'kkth';
-            div.innerHTML = '<div class="outer"><div class="inner">เสี่ยวๆๆๆๆ</div></div>';
+        var div = document.createElement('div');
+        document.body.appendChild(div);
+        // div.className = 'kkth';
+        div.innerHTML = '<div class="outer"><div class="inner">เสี่ยวๆๆๆๆ</div></div>';
