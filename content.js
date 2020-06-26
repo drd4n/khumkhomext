90717c7e1006f72ea42dcc550c97def2c376d3b4
Dan Kent <patorn.5@mail.kmutt.ac.th>
2020-06-26 23:56:38 +0700

    [update]make animation natural

--- content.js
+++ content.js
@@ -2,5 +2,5 @@
 
             var div = document.createElement('div');
             document.body.appendChild(div);
-            div.className = 'kkth';
-            div.innerHTML = '<span class="kkkk">เสี่ยวๆๆๆๆ</span>';
+            // div.className = 'kkth';
+            div.innerHTML = '<div class="kkth"><div class="kkkk">เสี่ยวๆๆๆๆ</div></div>';
