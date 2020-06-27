2388ea283b27f3168d574d304ec7f98e9f843815
poonynh_ <poon.roon3@mail.kmutt.ac.th>
2020-06-27 14:13:46 +0700

    [add] storage for send idletime
    can set on chrome.storage.sync
    but can't get() now

--- popup.js
+++ popup.js
@@ -2,4 +2,12 @@ $(function(){
     $('#urname').keyup(function(){
         $('#greeting').text('Hello ' + $('#urname').val())
     })
+    $('#savetime').click(function(){
+        var time = $('#sectime').val();
+        if(time){
+            chrome.storage.sync.set({'time':time},function(){
+                close();
+            });
+        }
+    });
 })
